0390ddab860d6f39bfbafabab49e0b6a5a8bb20e
unknown <vfurball@gmail.com>
2014-11-08 17:33:37 +0100

    initial video processing + sample video file

--- ObjectTracking/main.cpp
+++ ObjectTracking/main.cpp
@@ -1,4 +1,32 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
 int main()
 {
+	cv::VideoCapture capture;
+	capture.open("Wildlife.wmv");
+	cv::Mat frame;
+	//cv::namedWindow("Video", CV_WINDOW_AUTOSIZE);
+	while (capture.isOpened())
+	{
+		bool success = capture.read(frame);
+		if (!success)
+		{
+			break;
+		}
+		// show next frame
+		cv::imshow("Video", frame);
+
+		// break after pressing esc
+		if (cv::waitKey(30) == 27)
+		{
+			cv::destroyAllWindows();
+			break;
+		}
+	}
+	capture.release();
+	
+	std::cin.get();
+	std::cin.get();
 	return 0;
 }
