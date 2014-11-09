b20bba493fed8b3642de576e32c01ac8a755eac6
unknown <vfurball@gmail.com>
2014-11-09 21:01:04 +0100

    changed main to accomodate to tracker classes

--- ObjectTracking/main.cpp
+++ ObjectTracking/main.cpp
@@ -1,30 +1,25 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/gpu.hpp>
 #include <iostream>
+#include <vector>
+#include "tracker.h"
+#include "trackergpu.h"
 
 int main()
 {
-	cv::VideoCapture capture;
-	capture.open("Wildlife.wmv");
-	cv::Mat frame;
-	//cv::namedWindow("Video", CV_WINDOW_AUTOSIZE);
-	while (capture.isOpened())
-	{
-		bool success = capture.read(frame);
-		if (!success)
-		{
-			break;
-		}
-		// show next frame
-		cv::imshow("Video", frame);
+	// set number of threads during parallel computation
+	// doesnt work with TrackerGpu
+	/*cv::setNumThreads(1);
+	Tracker tracker("Wildlife.wmv", "chocs-front.jpg", 600);
+	tracker.track();*/
 
-		// break after pressing esc
-		if (cv::waitKey(30) == 27)
-		{
-			cv::destroyAllWindows();
-			break;
-		}
-	}
-	capture.release();
+	TrackerGpu tracker("Wildlife.wmv", "chocs-front.jpg", 600);
+	tracker.track();
 	
 	std::cin.get();
 	std::cin.get();
